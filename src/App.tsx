3cf4675437e63b8ee18514b7be7b5f0ca7e6c37c
Tiago Martins Vargas <vargas.tiago@hotmail.com>
2022-06-22 15:55:10 -0300

    Made API request using Apollo

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,16 @@
+import { gql, useQuery } from "@apollo/client"
+
+const GET_LESSONS_QUERY = gql`
+  query {
+    lessons {
+      id
+      title
+    }
+  }
+`
+
 function App() {
+  const { data } = useQuery(GET_LESSONS_QUERY)
   return (
     <h1>Hello</h1>
   )
