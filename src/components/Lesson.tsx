5bbe71cdf7a10a0f959a956980d71ff30e2ec993
Tiago Martins Vargas <vargas.tiago@hotmail.com>
2022-06-25 14:56:20 -0300

    Created routes

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,6 +1,7 @@
 import { CheckCircle, Lock } from 'phosphor-react'
 import { isPast, format } from 'date-fns'
 import ptBR from 'date-fns/locale/pt-BR'
+import { Link } from 'react-router-dom'
 
 interface LessonProps {
   title: string
@@ -16,11 +17,11 @@ export function Lesson({title, slug, availableAt, type}:LessonProps) {
   })
   
   return (
-    <a href="#">
+    <Link to={`/event/lesson/${slug}`} className='group'>
       <span className="text-gray-300">
         {availableDateFormatted}
       </span>
-      <div className="rounded border border-gray-500 p-4 mt-2">
+      <div className="rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500">
         <header className="flex items-center justify-between">
           {
             isLessonAvailable ? (
@@ -43,6 +44,6 @@ export function Lesson({title, slug, availableAt, type}:LessonProps) {
           {title}
         </strong>
       </div>
-    </a>
+    </Link>
   )
 }
